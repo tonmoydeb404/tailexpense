a8b52848d1de12ccb29cb0dba0c4091e679d4d0c
tonmoydeb404 <tonmoydeb404@gmail.com>
2024-12-22 22:57:45 +0600

    fixed use client string issue

--- app/views/dashboard/home/sections/budget-usage.tsx
+++ app/views/dashboard/home/sections/budget-usage.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
 
 import { Card, CardContent } from "~/components/ui/card";
--- app/views/dashboard/home/sections/monthly-usage.tsx
+++ app/views/dashboard/home/sections/monthly-usage.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Label, Pie, PieChart } from "recharts";
 
 import { Card, CardContent, CardFooter } from "~/components/ui/card";
