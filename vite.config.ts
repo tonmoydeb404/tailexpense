bedeb32d3797168c0c1d3a898266313ed215c705
tonmoydeb404 <tonmoydeb404@gmail.com>
2024-12-22 23:32:36 +0600

    updated deployment files

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import netlifyPlugin from "@netlify/vite-plugin-react-router";
 import { reactRouter } from "@react-router/dev/vite";
 import autoprefixer from "autoprefixer";
 import tailwindcss from "tailwindcss";
@@ -10,5 +11,5 @@ export default defineConfig({
       plugins: [tailwindcss, autoprefixer],
     },
   },
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [reactRouter(), tsconfigPaths(), netlifyPlugin()],
 });
