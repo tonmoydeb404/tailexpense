b34604271de327faefea283d839db61f3cee0abc
tonmoydeb404 <tonmoydeb404@gmail.com>
2024-12-22 16:21:37 +0600

    updated sidebar responsiveness

--- app/components/layouts/dashboard/sidebar.tsx
+++ app/components/layouts/dashboard/sidebar.tsx
@@ -15,14 +15,25 @@ import { cn } from "~/lib/utils";
 import { generalLinks, systemLinks } from "./config";
 
 const DashboardSidebar = () => {
-  const { state, open } = useSidebar();
+  const { state, openMobile, openTab } = useSidebar();
+  const isOpen = state !== "collapsed";
+
   return (
     <Sidebar collapsible="icon">
-      <SidebarHeader className={cn(open ? "px-8 pt-10" : "", "pb-10")}>
-        {open && <img src="/brand/logo-full.svg" className="w-full" />}
-        {!open && <img src="/brand/logo-mini.svg" className="w-8 max-w-full" />}
+      <SidebarHeader
+        className={cn(
+          isOpen || openMobile || openTab ? "px-8 pt-10" : "",
+          "pb-10"
+        )}
+      >
+        {(isOpen || openMobile || openTab) && (
+          <img src="/brand/logo-full.svg" className={cn("w-full")} />
+        )}
+        {!isOpen && !openMobile && !openTab && (
+          <img src="/brand/logo-mini.svg" className="w-8 max-w-full" />
+        )}
       </SidebarHeader>
-      <SidebarContent className={open ? "px-3" : ""}>
+      <SidebarContent className={isOpen ? "px-3" : ""}>
         <SidebarGroup>
           <SidebarGroupLabel>General</SidebarGroupLabel>
           <SidebarGroupContent>
@@ -31,7 +42,7 @@ const DashboardSidebar = () => {
                 <SidebarMenuItem key={item.title}>
                   <SidebarMenuButton
                     asChild
-                    size={open ? "lg" : "default"}
+                    size={isOpen ? "lg" : "default"}
                     className="text-base px-4"
                   >
                     <Link to={item.url}>
@@ -52,7 +63,7 @@ const DashboardSidebar = () => {
                 <SidebarMenuItem key={item.title}>
                   <SidebarMenuButton
                     asChild
-                    size={open ? "lg" : "default"}
+                    size={isOpen ? "lg" : "default"}
                     className="text-base px-4"
                   >
                     <Link to={item.url}>
