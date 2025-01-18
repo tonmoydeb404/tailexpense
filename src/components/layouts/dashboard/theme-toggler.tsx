83fb43a70f345ce9529b666c037ecb78d38967e3
tonmoydeb404 <tonmoydeb404@gmail.com>
2025-01-18 23:55:41 +0600

    refactor theme context: simplify theme management and update to use toggle function; enhance SEO with meta tags and manifest

--- src/components/layouts/dashboard/theme-toggler.tsx
+++ src/components/layouts/dashboard/theme-toggler.tsx
@@ -1,6 +1,6 @@
 import { LucideMoon, LucideSun } from "lucide-react";
 import { SidebarMenuButton, SidebarMenuItem } from "~/components/ui/sidebar";
-import { useTheme } from "~/contexts/theme";
+import { useTheme } from "~/contexts/theme/index";
 
 type Props = {
   isSidebarOpen: boolean;
@@ -8,13 +8,13 @@ type Props = {
 
 const ThemeToggler = (props: Props) => {
   const { isSidebarOpen } = props;
-  const { setTheme, theme } = useTheme();
+  const { toggleTheme, theme } = useTheme();
   return (
     <SidebarMenuItem>
       <SidebarMenuButton
         size={isSidebarOpen ? "lg" : "default"}
         className="text-base px-4"
-        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        onClick={toggleTheme}
       >
         {theme === "dark" ? <LucideSun /> : <LucideMoon />}
         <span>Theme</span>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import { ManifestOptions, VitePWA } from "vite-plugin-pwa";
 import tsconfigPaths from "vite-tsconfig-paths";
+import manifest from "./public/manifest.json";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: { enabled: false },
+      manifest: manifest as ManifestOptions,
+      manifestFilename: "manifest.json",
+    }),
+  ],
 });
