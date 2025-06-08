4365fdfcd0b5ec5b80e49409ae552aa06d20eb35
tonmoydeb404 <tonmoydeb404@gmail.com>
2025-06-08 15:04:40 +0600

    fix: adjust AppBar NavLink layout and remove hidden title span

--- src/components/layouts/dashboard/app-bar.tsx
+++ src/components/layouts/dashboard/app-bar.tsx
@@ -10,11 +10,10 @@ const AppBar = (props: Props) => {
         <NavLink
           to={item.url}
           key={item.url}
-          className="size-10 [&_svg]:size-4 flex items-center justify-center hover:bg-accent hover:text-accent-foreground [&.active]:w-auto [&.active]:px-3 [&.active]:gap-2 [&.active_span]:inline-block rounded-lg [&.active]:bg-primary [&.active]:text-primary-foreground duration-200"
+          className="size-10 [&_svg]:size-4 flex flex-col items-center justify-center hover:bg-accent hover:text-accent-foreground [&.active]:w-auto [&.active]:px-3 rounded-lg [&.active]:bg-primary [&.active]:text-primary-foreground duration-200"
           end
         >
           <item.icon />
-          <span className="text-xs font-medium hidden">{item.title}</span>
         </NavLink>
       ))}
     </div>
